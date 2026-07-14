56380b04933dd33dea7240fa0be9a2a369c575e4
Hiran-Joy <hiranjoy7@gmail.com>
2026-07-14 20:58:51 +0530

    Adding AddTeam.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './App.css';
+import AddTeam from './components/AddTeam';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <AddTeam />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
