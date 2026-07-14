46b80251cd01bc926ea3c7e5560a552295ab539e
Hiran-Joy <hiranjoy7@gmail.com>
2026-07-14 21:20:31 +0530

    NavBar Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <NavigationBar />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
